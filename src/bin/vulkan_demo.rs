5fb7a819bb05279136d4947d63a828d99c2eb52c
Zoe McCarthy <zoemccarthy12@gmail.com>
2020-07-12 01:22:48 -0500

    Finished vulkano demo including mandelbrot.  Skipping graphics pipeline for now, moving on to raytracing.

--- src/bin/vulkan_demo.rs
+++ src/bin/vulkan_demo.rs
@@ -11,16 +11,10 @@ use vulkano::sync::GpuFuture;
 use vulkano::pipeline::ComputePipeline;
 use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
 use vulkano::descriptor::PipelineLayoutAbstract;
-use vulkano::format::{Format, ClearValue};
+use vulkano::format::{Format};
 use vulkano::image::{Dimensions, StorageImage};
 
 use image::{ImageBuffer, Rgba};
-
-
-struct BasicStruct {
-    a: u32,
-    b: bool,
-}
 
 
 fn main()  -> std::io::Result<()> {
@@ -45,55 +39,9 @@ fn main()  -> std::io::Result<()> {
 
     let queue = queues.next().unwrap();
 
-
-    let data = BasicStruct { a: 5, b: true };
-    let buffer = CpuAccessibleBuffer::from_data(device.clone(),
-                                                BufferUsage::all(), false, data)
-        .expect("failed to create buffer");
-
-
-
-    let mut content = buffer.write().unwrap();
-
-    content.a *= 2;
-    content.b = false;
-
-
-    let iter = (0 .. 128).map(|_| 5u8);
-    let buffer = CpuAccessibleBuffer::from_iter(device.clone(),
-    BufferUsage::all(), false, iter).unwrap();
-
-    let mut content = buffer.write().unwrap();
-
-    content[12] = 83;
-    content[7] = 3;
-
-    let source_content = 0 .. 64;
-    let source = CpuAccessibleBuffer::from_iter(device.clone(),
-                                                BufferUsage::all(), false,
-                                                source_content)
-        .expect("failed to create buffer");
-
-
-    let dest_content = (0 .. 64).map(|_| 0);
-    let dest = CpuAccessibleBuffer::from_iter(device.clone(),
-                                                BufferUsage::all(), false,
-                                              dest_content)
-        .expect("failed to create buffer");
-
-    let mut builder = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap();
-    builder.copy_buffer(source.clone(), dest.clone()).unwrap();
-    let command_buffer = builder.build().unwrap();
-
-    let finished = command_buffer.execute(queue.clone()).unwrap();
-
-    finished.then_signal_fence_and_flush().unwrap()
-        .wait(None).unwrap();
-
-    let src_content = source.read().unwrap();
-    let dest_content = dest.read().unwrap();
-    //println!("{:#?}", &*dest_content);
-    assert_eq!(&*src_content, &*dest_content);
+    let image = StorageImage::new(device.clone(),
+                                  Dimensions::Dim2d { width: 1024, height: 1024},
+                                  Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
 
     mod cs {
         vulkano_shaders::shader!{
@@ -101,16 +49,31 @@ fn main()  -> std::io::Result<()> {
         src: "
         #version 450
 
-        layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+        layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
-        layout(set = 0, binding = 0) buffer Data {
-            uint data[];
-        } buf;
+        layout(set = 0, binding = 0, rgba8) uniform writeonly image2D img;
 
         void main() {
-            uint idx = gl_GlobalInvocationID.x;
-            buf.data[idx] *= 12;
+            vec2 norm_coordinates = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(imageSize(img));
+            vec2 c = (norm_coordinates - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
+
+            vec2 z = vec2(0.0, 0.0);
+            float i;
+            for (i = 0.0; i < 1.0; i += 0.005) {
+                z = vec2(
+                    z.x * z.x - z.y * z.y + c.x,
+                    z.y * z.x + z.x * z.y + c.y
+                );
+
+                if (length(z) > 4.0) {
+                    break;
+                }
+            }
+
+            vec4 to_write = vec4(vec3(i), 1.0);
+            imageStore(img, ivec2(gl_GlobalInvocationID.xy), to_write);
         }
+
         "
         }
     }
@@ -119,53 +82,39 @@ fn main()  -> std::io::Result<()> {
         .expect("failed to create shader module");
 
 
-    let compute_pipeline = Arc::new(ComputePipeline::new(device.clone(), &shader.main_entry_point(), &())
-                                        .expect("failed to create compute pipeline"));
-
-    let data_iter = 0..65536;
-    let data_buffer =
-        CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
-                                       false, data_iter)
-        .expect("failed to create buffer");
-
-    let layout = compute_pipeline.layout().descriptor_set_layout(0).unwrap();
-    let set = Arc::new(PersistentDescriptorSet::start(layout.clone())
-        .add_buffer(data_buffer.clone()).unwrap()
-        .build().unwrap()
+    let compute_pipeline = Arc::new(
+        ComputePipeline::new(device.clone(), &shader.main_entry_point(), &())
+            .expect("failed to create compute pipeline"),
     );
 
+    let set = Arc::new(
+        PersistentDescriptorSet::start(
+            compute_pipeline
+                .layout()
+                .descriptor_set_layout(0)
+                .unwrap()
+                .clone(),
+        )
+            .add_image(image.clone())
+            .unwrap()
+            .build()
+            .unwrap(),
+    );
 
-    let mut builder = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap();
-    builder.dispatch([1024, 1, 1], compute_pipeline.clone(), set.clone(), ()).unwrap();
-    let command_buffer = builder.build().unwrap();
-
-    let finished = command_buffer.execute(queue.clone()).unwrap();
-
-    finished.then_signal_fence_and_flush().unwrap()
-        .wait(None).unwrap();
-
-    let content = data_buffer.read().unwrap();
-    for (n, val) in content.iter().enumerate() {
-        assert_eq!(*val, n as u32 * 12);
-    }
-
-    println!("Everything Succeeded");
-
-
-    let image = StorageImage::new(device.clone(),
-                                  Dimensions::Dim2d { width: 1024, height: 1024},
-    Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
-
-    let buf =
-        CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
-                                       false,
-                                       (0 .. 1024 * 1024 * 4).map(|_| 0u8))
-            .expect("failed to create buffer");
+    let buf = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        false,
+        (0..1024 * 1024 * 4).map(|_| 0u8),
+    )
+        .expect("failed to create buffer");
 
     let mut builder = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap();
     builder
-        .clear_color_image(image.clone(), ClearValue::Float([0.0, 0.0, 1.0, 1.0])).unwrap()
-        .copy_image_to_buffer(image.clone(), buf.clone()).unwrap();
+        .dispatch([1024 / 8, 1024 / 8, 1], compute_pipeline.clone(), set.clone(), ())
+        .unwrap()
+        .copy_image_to_buffer(image.clone(), buf.clone())
+        .unwrap();
     let command_buffer = builder.build().unwrap();
 
     let finished = command_buffer.execute(queue.clone()).unwrap();
@@ -178,6 +127,7 @@ fn main()  -> std::io::Result<()> {
 
     image.save("./output/image.png").unwrap();
 
+    println!("Everything Succeeded");
 
     Ok(())
 }
